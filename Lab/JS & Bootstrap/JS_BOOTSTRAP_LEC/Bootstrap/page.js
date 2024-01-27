53116562dad9c2424be6d0cf17d56eefa6d5d35e
andy489 <stoev.andy@gmail.com>
2024-01-27 17:59:14 +0200

    ref

--- Lab/JS & Bootstrap/JS_BOOTSTRAP_LEC/Bootstrap/page.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function() {
-//     $('#my-btn').click(function() {
-//         $('#my-txt').text('My custom text! Jquery!').addClass('text-primary')
-//     })
-// });
-
-function myCustomClick() {
-    console.log("Custom click!")
-}
